d51e752a83daeda9ad70bb3de6c156c9b4d0e9ca
Shirong Tang <tangshirong477@gmail.com>
2024-05-10 04:48:37 -0400

    nav bar in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,24 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
+import { Button } from './Button';
 
 function NavBar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu= () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 968){
+      setButton(false);
+    }else{
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize',showButton);
 
   return (
     <>
@@ -49,6 +62,7 @@ function NavBar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>LOG IN</Button>}
         </div>
     </nav>
     </>
